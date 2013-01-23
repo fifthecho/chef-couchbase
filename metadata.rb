41bc3e2e584c1b431182026116b7e2f90f78b177
Jeff Moody <fifthecho@gmail.com>
2013-01-23 10:22:54 -0500

    Added distro support lines

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,18 @@
 name             "couchbase"
-maintainer       "getaroom"
-maintainer_email "devteam@roomvaluesteam.com"
+maintainer       "getaroom", "fifthecho"
+maintainer_email "devteam@roomvaluesteam.com", "jmoody@datapipe.com"
 license          "MIT"
 description      "Installs/Configures Couchbase"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "1.0.0"
+version          "1.0.1"
 
 supports "debian"
 supports "ubuntu"
+supports "centos"
+supports "redhat"
+supports "fedora"
+supports "scientific"
+supports "amazon"
 
 recipe "couchbase::server", "Installs couchbase-server"
 recipe "couchbase::client", "Installs libcouchbase"
